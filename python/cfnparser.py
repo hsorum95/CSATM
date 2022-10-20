dab96ce31f69023f2d4c8435cb3b86ab682d3dec
Håkon Sørum <hsorum95@gmail.com>
2022-10-20 19:25:39 +0200

    created resource dataclass and create list of all resources

--- python/cfnparser.py
+++ python/cfnparser.py
@@ -1,5 +1,6 @@
 import sys
 import ruamel.yaml
+from dataclasses import dataclass
 
 
 class Generic:
@@ -53,24 +54,51 @@ def default_constructor(constructor, tag_suffix, node):
     state = generic.construct(constructor, node)
     instance.__init__(tag_suffix, state, style=style)
 
+@dataclass
+class Resource:
+    name: str
+    type: str
+    trust_boundry: str
+
 
 ruamel.yaml.add_multi_constructor('', default_constructor, Loader=ruamel.yaml.SafeLoader)
-
-
 yaml = ruamel.yaml.YAML(typ='safe', pure=True)
 yaml.default_flow_style = False
 yaml.register_class(GenericScalar)
 yaml.register_class(GenericMapping)
 yaml.register_class(GenericSequence)
 
-input_file = input('Where is the cloudformation-template located?')
-with open(input_file, 'r') as stream:
-    raw = stream.read()
+def get_cloudformationfile():
+    input_file = input('Where is the cloudformation-template located?')
+    with open(input_file, 'r') as stream:
+        raw = stream.read()
+    base = yaml.load(raw)
+    return base
 
-base = yaml.load(raw)
-print((base))
-lst = base['Resources']['PharmacyVPC']['Properties']['Tags']
-dict = lst[0]
-print(dict.get('Key'))
-print(dict.get('Value'))
+
+def get_resources(input_dict: dict ) -> list:
+    resource_dict = []
+    section_id = 'Resources'
+    for key in input_dict:
+        if key == section_id:
+            for sub_key in input_dict[section_id]:
+                tmp = Resource(sub_key,input_dict[section_id].get(sub_key,{}).get('Type'), "cloudnetwork")
+                resource_dict.append(tmp)
+    return resource_dict
+
+yaml_dumped_to_dct: dict = get_cloudformationfile()
+
+recources_in_list: list = get_resources(yaml_dumped_to_dct)
+
+for item in recources_in_list:
+    print(item.name)
+    print(item.type)
+    print(item.trust_boundry)
+
+# lst = base['Resources']['PharmacyVPC']['Properties']['Tags']
+# dict = lst[0]
+# key = dict.get('Key')
+# value = dict.get('Value')
+# tuple = (key, value)
+#print(tuple)
 #print((base['Resources']['PharmacyVPC']['Properties']['Tags']))
--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
-from cfnconverter import *
+from cfnparser import *
 import attacktrees
 
 # /home/hsorum95/CSATM/cloudformation/three-tier-web-app.yml
 ordered_dict = get_cloudformationfile()
 output = get_resources(ordered_dict)
-print(ordered_dict.keys())
+print(ordered_dict)
